ab5caad831026b739d7ffd431625a41feca9c377
AbdeLilah <contactme.abdelilah@gmail.com>
2024-11-28 16:13:12 +0100

    ChariTeam Update is Uploaded

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ function click_dropdown_btn() {
         dropdown_lick = false;
     }
 }
+
+let firstPrice = document.getElementById("first-price");
+let secondPrice = document.getElementById("second-price");
+let therdPrice = document.getElementById("therd-price");
+function price_select(id) {
+    console.log(id);
+    if (id == 10) {
+        firstPrice.classList.add("active");
+        secondPrice.classList.remove("active");
+        therdPrice.classList.remove("active");
+    }
+    else if (id == 20) {
+        firstPrice.classList.remove("active");
+        secondPrice.classList.add("active");
+        therdPrice.classList.remove("active");
+    }
+    else if (id == 30) {
+        firstPrice.classList.remove("active");
+        secondPrice.classList.remove("active");
+        therdPrice.classList.add("active");
+    }
+}
